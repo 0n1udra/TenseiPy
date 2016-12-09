7eef99f3efcaa83eca368fb10cc60d9ac5ba4cef
Drake Thomas <drakethomas550@gmail.com>
2016-12-09 15:06:46 -0500

    changes

--- DT_S_lib.py
+++ DT_S_lib.py
@@ -6,27 +6,21 @@ def game_Menu(x):
     for i in range(len(x)):
         print(str(i) + ')', x[i])
 
-
-
 # Screen interaction
-def clear():
-    print("\n"*50) # clears the screen
+def clear(): print("\n"*50) # clears the screen
 
 # pauses game
-def wait():
-    input(" :") # pauses game, until press 'enter'
+def wait(): input(" :") # pauses game, until press 'enter'
 
 # game back and main menu options
 def print_Back():
     print("99 ) Go back")
     print("100 ) Main menu") # prints menu options
+
 def input_Back(x, pos): # check if input is 99 to go back, or 100 to go to main menu
-    if x == 99:
-        pos()
-    elif x == 100:
-        Startup()
-    else:
-        pass
+    if x == 99: pos()
+    elif x == 100: Startup()
+    else: pass
 
 # loading screen
 def loading_Screen(x):
@@ -42,19 +36,14 @@ def loading_Screen(x):
 def game_Over(): # if you die, this asks you if you want to play again
     inp = input("Would you like to continue? > ")
     try: # this is if somebody inputs something that is not usable it goes to except, instead of crasing
-        if inp in ('y', 'yes'):
-            Startup() # if inputted 'y' or 'yes' it goes back to main menu
+        if inp in ('y', 'yes'): Startup() # if inputted 'y' or 'yes' it goes back to main menu
         else: # if anything else, it quits the game
             print("Thanks for playing!")
             exit() # if you don't continue, it says bye
-    except:
-        pass
+    except: pass
 
-def error():
-    input("That didn't work : ") # simple error message
+def error(): input("That didn't work : ") # simple error message
 
 # makes sure this program doesn't run, if runned directly
-if __name__ == '__main__':
-    print("Shuld not be runned directly!")
-else:
-    print("Drake's Standard Library... Imported.")
+if __name__ == '__main__': print("Shuld not be runned directly!")
+else: print("Drake's Standard Library... Imported.")
