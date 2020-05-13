68317830240c0372027e26a341ec01a0e8fea1cf
Drake Thomas <drake550@pm.me>
2020-05-12 21:31:09 -0400

    finally fixed double inventory bug
    fixed bug with even more bodge code!

--- Slime/Tensei.py
+++ Slime/Tensei.py
@@ -8,12 +8,16 @@ from sleep import *
 # ASCII Art
 import ascii
 
+ascii.slime = ''
+ascii.great_sage = ''
+
 # Sleep times
 t2, t3, t4, t5 = 2, 3, 4, 5
 
 def sprint(sleepTime, Msg):
     print(Msg)
     sleep(sleepTime)
+# TODO add disable sleep function natively
 
 class Scene_Template:
 
@@ -21,36 +25,26 @@ class Scene_Template:
     def __init__(self):
         self.SceneStart()
 
+    def runFuncs(self, usrInp, actions, funcs):
+        for i in range(len(funcs)):
+            for j in actions[i]:
+                if usrInp in j:
+                    funcs[i]()
+                    return
+
     def actionMenu(self, msg, actions, funcs):
-
-        menuActions = [['stats', 'attributes', 'attrs'], ['inventory', 'inv', 'stomach']]
+        menuActions = [['stat', 'stats', 'attributes', 'attrs', 'attr'], ['storage', 'inventory', 'inv', 'stomach']]
+        menuActionsFlat = [val for sub in menuActions for val in sub]
         menuFuncs = [slime.showAttributes, slime.showInventory]
-        # Runs through each function available
-        while True:
-            print("\nAvailable Actions:", msg, ' | stats, inv')
-            self.usrInp = input("\n> ").lower()
-            for i in range(len(menuFuncs)):
-                for j in menuActions[i]:
-                    if self.usrInp in j:
-                        menuFuncs[i]()
-                else:
-                    for i in range(len(funcs)):
-                        for j in actions[i]:
-                            if self.usrInp in j:
-                                funcs[i]()
-                                return
+        print("\nAvailable Actions:", msg, ' | stats, inv')
+        self.usrInp = input("\n> ").lower()
+        while self.usrInp in menuActionsFlat:
+            self.runFuncs(self.usrInp, menuActions, menuFuncs)
+            self.actionMenu(msg, actions, funcs)
+        else:
+            self.runFuncs(self.usrInp, actions, funcs)
 
-
-
-
-
-    def init(self):
-        pass
-
-    def SceneStart(self):
-        pass
-
-
+# Manga, Chapter 1
 class Scene_Intro(Scene_Template):
 
     def sleep(self): pass
@@ -62,24 +56,16 @@ class Scene_Intro(Scene_Template):
 
         sprint(t3, "<<Confirmation Complete. Constructing a body that does not require blood...>>")
         #TODO Add Skill
-
         sprint(t2, "<<Confirmation Complete. Acquiring Skill [Predator]...>>")
         slime.addAttribute(Predator_Skill())
-        sleep(t2)
-
         sprint(t2, "<<Confirmation Complete. Acquiring extra skill [Sage]...>>")
-        sleep(t2)
         slime.addAttribute(Sage_Skill())
-        sleep(t2)
-        
         sprint(t2, "<<Confirmation Complete. Extra skill [Sage] evolving.>>")
         sprint(t2, '...')
         slime.skillUpgrade(Sage_Skill(), Great_Sage_Skill())
         sleep(t3)
 
-        print(slime.showAttributes())
-        sleep(t3)
-
+        sprint(t3, slime.showAttributes())
 
         sprint(t3, "\nYou wake up, or at least you think you are 'awake'.")
         sprint(t2, "It's so dark, where is this.")
@@ -89,8 +75,6 @@ class Scene_Intro(Scene_Template):
         sprint(t2, "That said is this the hospital bed?")
         sprint(t3, "I can't see anything, I can't hear anything.")
         sprint(t3, "Is it already past the curfew? I should First call the nurse...")
-
-
 
         self.actionMenu("Move Arms, Move Legs",
                         [['move arms', 'twitch arms'], ['move legs']],
@@ -108,7 +92,6 @@ class Scene_Intro(Scene_Template):
         sprint(t3, "Alright, Let's try to taste it")
         sprint(t3, "Actually! Where the fuck is my mouth?")
         sprint(t3, "The grass melted. Is it being absorbed after melting...?")
-
         sprint(t3, "WAIT A MINUTE, this isn't even human anymore!!?!")
         sprint(t3, "Eh.. Wait a moment... Let's calm down and confirm my appearance.")
 
@@ -120,8 +103,10 @@ class Scene_Intro(Scene_Template):
         sprint(t3, "Wait what kind of joke is this! Who would accept something like this!!")
         sprint(t4, "ahhhh... but... dissolving and absorbing plants, this streamlined body shape.")
         sprint(t3, "And this sort of elastic feeling")
+
         sprint(t4, "***Although Minami Satoru didn't want to admint it.***")
         sprint(t3, "***He has reincarrnated into a slime!***")
+
         sprint(t3, ascii.slime)
 
 
@@ -138,13 +123,11 @@ class Scene_Intro(Scene_Template):
                         [self.eatGrass, self.puyo])
 
         sprint(t4, "I've ate what seems like a lot of grass, and yet I haven't pooped yet")
+        slime.addInventory(Grass_Item(), capacity=0.9)
         sprint(t3, "So where did all the grass go?")
         sprint(t4, "<<Answer. They are stored inside the unique skill [Predator]'s stomach sack.>>")
         sprint(t3, "Whoa, somebody actually answered!?!")
         sprint(t2, "<<Also, the current spaced used is less than 1%.>>")
-        sleep(t3)
-        slime.addInventory(Grass_Item(), capacity=0.9)
-
         sprint(t2, "I've heard this before, this voice that sounded computer synthesized....")
         sprint(t2, "W-w-w-who is it?")
         sprint(t3, "<<Answer. This is the unique skill [Great Sage]'s effect.>>")
@@ -153,7 +136,6 @@ class Scene_Intro(Scene_Template):
         sprint(t2, "Unique skill [Great Sage] heh?")
         sprint(t3, "Speakin of which, when I died I seemed to have acquired some of of skill")
         sprint(t3, "That said, what are skills?")
-
         sprint(t5, "<<Answer. When growth is recognized by the world, occasionally one will obtain a [Skill].>>")
         sprint(t4, "Although I don't understand it too much... It seems like that this is just how this world is.")
         sprint(t2, "Are both [Great Sage] and [Predator] my skills?")
@@ -180,32 +162,30 @@ class Scene_Intro(Scene_Template):
         sprint(t3, "But how do I even reply?")
 
 
+
     
 
+        self.actionMenu('Puyo!',
+                        ['move', 'wonder'],
+                        [self.eatGrass, self.puyo])
 
     def eatGrass(self):
         print("Ooooweeee more grass!")
 
 
     def squash(self):
-        print("hehhhh")
-        sleep(1)
-        print("Is that so....")
-        sleep(1)
-        print("hmmmmmm, mhmmmm")
-        sleep(1)
+        sprint(t2, "hehhhh")
+        sprint(t2, "Is that so....")
+        sprint(t2, "hmmmmmm, mhmmmm")
 
     def puyo(self):
-        print("Puyo!")
-        sleep(1)
+        sprint(t2, "Puyo!")
 
     def MoveLegs(self):
-        print("You can't feel any legs to move, what is happening?")
-        sleep(t3)
+        sprint(t2, "You can't feel any legs to move, what is happening?")
 
     def MoveArms(self):
-        print("Where are my arms, I can't feel them!")
-        sleep(t3)
+        sprint(t2, "Where are my arms, I can't feel them!")
 
 slime = Inventory()
 start = Scene_Intro()
