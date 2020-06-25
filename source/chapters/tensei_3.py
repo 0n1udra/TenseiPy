09b475eaa9e437171b6ff375fc5f9f3bdde0414a
Drake Thomas <drake550@pm.me>
2020-06-24 22:13:04 -0400

    story update chapter 3

--- source/chapters/tensei_3.py
+++ source/chapters/tensei_3.py
@@ -2,7 +2,24 @@ from start_game import *
 def Chapter3(rimuru):
     def StartChapter3():
 
-        sprint("TEST")
+        ActionMenu(['*Use Sticky Thread', '*Move'],
+                [['use sticky thread'], ['move']],
+                [StickySwing, move])
+
+        ssprint("*The little slime swings from tree to tree with [Sticky Thread].*")
+        ssprint("*While practicing pronunciation with [Ultrasound Waves]. A pack of Star Wolfs shows up")
+
+
+
+        # ========== Goblins 
+
+
+    def StickySwing():
+        sprint("This is fun, but I still don't know what I'm looking for.")
+
+    def move():
+        sprint("Where am I going?")
+
 
     StartChapter3()
 
