faad818b6c957c64ef6bc8bfba11666cbedb3c02
Drake Thomas <drake550@pm.me>
2020-07-04 04:21:48 -0400

    progressed story a bit. still need to remodel this one tho.

--- source/chapters/tensei_3.py
+++ source/chapters/tensei_3.py
@@ -1,75 +1,64 @@
 from start_game import *
 def Chapter3(rimuru):
-    def StartChapter3():
+    class Chapter_3:
+        def __init__(self):
+            ssprint("*While practicing pronunciation with [Ultrasound Waves]. A pack of Star Wolfs shows up*")
+            ssprint("Was I that loud... Eh? Where are those wolves going? What's this...")
+            sprint("You strong one.")
+            ssprint("Wait, are those... Goblins? Also, How can I understand them?")
+            ssprint("<<Answer, [Magic Perception] converts sound waves to comprehensible sentences.>>")
+            ssprint("<<Also, sound waves can also be used to communicate your thoughts.>>")
+            ssprint("Is that so, let's try that.")
 
-        ActionMenu(['*Use Sticky Thread', '*Explore'],
-                [['use sticky thread'], ['explore']],
-                [StickySwing1, Explore])
-
-        ssprint("*While practicing pronunciation with [Ultrasound Waves]. A pack of Star Wolfs shows up*")
-        ssprint("Was I that loud... Eh? Where are those wolves going? What's this...")
-        sprint("You strong one.")
-        ssprint("Wait, are those... Goblins? Also, How can I understand them?")
-        ssprint("<<Answer, [Magic Perception] converts sound waves to comprehensible sentences.>>")
-        ssprint("<<Also, sound waves can also be used to communicate your thoughts.>>")
-
-        ActionMenu(['*Speak to Goblins', '*Chase Wolves', '*Run'],
-                [['speak to goblins'], ['chase wolves'], ['run']],
-                [Goblins, Wolves, Run])
-        ssprint("Is that so, let's try that.")
+            ActionMenu(self)
+         #   ActionMenu(['*Speak to Goblins', '*Chase Wolves', '*Run'],
+         #           [['speak to goblins'], ['chase wolves'], ['run']],
+         #           [Goblins, Wolves, Run])
 
 
 
+
+
+        def _use_sticky_thread(self):
+            ssprint("Swinging from tree to tree with [Sticky Thread] seems to be a pretty effective way to travel.")
+
+        def _explore_more(self):
+            sprint("Where am I going?")
+
+        def test():
+            print("oh:")
 
     # ========== Goblins 
-    def Goblins():
-        ActionMenu(['*Friendly', '*Mad', '*Coerce', '*Attack'],
-                [['friendly'], ['mad'], ['coerce'], ['attack #$%']],
-                [FriendlyGoblin, MadGoblin, CoerceGoblin, AttackGoblin])
+    class Goblins_Path(Chapter_3):
+        def __init__(self):
+            ssprint("How should I introduce myself?")
 
-        sprint("HELLO, I'M A SLIME. MY NAME IS RIMURU")
-        sprint("...")
-        sprint("Strong one we have already recognized your strength. Please, lower your voice!")
-        ssprint("Oh. I guess my thoughts were to loud?")
-        sprint("Sorry, I am still adjusting.")
-        sprint("T-There is no need to apologize strong one!")
-        sprint("Ok, I wasn't planning on doing anything ahead of here. Is there anything you need from me?")
-        sprint("No, that is our village ahead of here. We felt a strong demonic aura and decided to immediately investigate.")
-        ssprint("Demonic aura? where?")
-        sprint("I didn't feel.")
+      #  ActionMenu(['*Friendly', '*Mad', '*Coerce', '*Attack'],
+      #          [['friendly'], ['mad'], ['coerce'], ['attack #$%']],
+      #          [FriendlyGoblin, MadGoblin, CoerceGoblin, AttackGoblin])
 
-    def FriendlyGoblin():
-        pass
-
-    def MadGoblin():
-        pass
-
-    def CoerceGoblin():
-        pass
-
-    def AttackGoblin():
-        pass
+        def be_friendly(self):
+            sprint("HELLO, I'M A SLIME. MY NAME IS RIMURU")
+            sprint("...")
+            sprint("Strong one we have already recognized your strength. Please, lower your voice!")
+            ssprint("Oh. I guess my thoughts were to loud?")
+            sprint("Sorry, I am still adjusting.")
+            sprint("T-There is no need to apologize strong one!")
+            sprint("Ok, I wasn't planning on doing anything ahead of here. Is there anything you need from me?")
+            sprint("No, that is our village ahead of here. We felt a strong demonic aura and decided to immediately investigate.")
+            ssprint("Demonic aura? where?")
+            sprint("I didn't feel.")
 
 
     # ========== Wolves
-    def Wolves():
+    class Chase_Wolves:
         pass
 
 
     # ========== Run
-    def Run():
+    def run_away(self):
         pass
 
 
-    def StickySwing1():
-        ssprint("Swinging from tree to tree with [Sticky Thread] seems to be a pretty effective way to travel.")
-
-    def StickySwing2():
-        sprint("Weeeeee")
-
-    def Explore():
-        sprint("Where am I going?")
-
-
-    StartChapter3()
+    Chapter_3()
 
