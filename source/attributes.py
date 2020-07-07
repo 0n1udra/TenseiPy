f3f07676d0cd6a1295db7f18b419c4e7c3628b8f
Drake Thomas <drake550@pm.me>
2020-07-07 01:12:11 -0400

    Re-added use_skill function, mimic functions seems to be worrking

--- source/attributes.py
+++ source/attributes.py
@@ -30,17 +30,13 @@ class Attributes:
             .attributes_generator('ranga', True)
         """
 
-        # Specify character other than Rimuru.
-        if character:
-            character = self.get_object(character).attributes
-        else:
-            character = self.attributes
-
-        for skill_type, skills in character.items():
+        for skill_type, skills in character.attributes.items():
             if output and skills:
-                # Prints out skill category (Ultimate, Unique, etc) type also for printing in game.
-                yield f'{skill_type}:'
+                # Prints out skill category (Ultimate, Unique, etc)
+                yield f'~{skill_type}~'
+
             for skill_name, skill_object in skills.items():
+                # Prints if skill is active or passive
                 if output:
                     if skill_object.active:
                         yield f'\t{skill_name} (Active)'
@@ -66,25 +62,24 @@ class Attributes:
         """
 
         # If no character was specified, else show player stats (rimuru)
-
-        try:
+        character = self
+        if self.is_str(character):
             character = self.get_object(character)
-        except:
-            pass
 
-        print(f"""
-  -----Attributes/Skills-----
-  Name: {self.name} {self.family_name}""")
+
+        print("-----Attributes/Skills-----")
+        print(f"Name: {self.name} {self.family_name}\n")
 
         for i in self.attributes_generator(character, output=True):
             print(i)
 
         # Only shows mimicry info when not looking at stats of other monsters and is currently using mimicry
         if self.current_mimic:
-            print("\n\t-----Mimicry-----")
+            print("\n-----Mimicry-----")
             print(f"Mimicking: {character.current_mimic.name}\n")
-            for j in self.attributes_generator(characterr.current_mimic, True):
-                print(f'\t{j}')
+            for j in self.attributes_generator(character.current_mimic, True):
+                print(f'{j}')
+        print()
 
     def add_attribute(self, attribute, show_acquired_msg=True, show_skill_info=False):
         """
@@ -122,7 +117,7 @@ class Attributes:
 
         try:
             attribute = self.get_object(attribute)
-            del self.attributes[attribute.attributeLevel][attribute.name]
+            del self.attributes[attribute.skill_level][attribute.name]
         except:
             print("ERROR Deleting attribute. If you're seeing this message, please let developer know")
 
@@ -145,12 +140,12 @@ class Attributes:
             ssprint(f'<<{skill_from.skill_level} [{skill_from}] evolving to {skill_to.skill_level} [{skill_to}]...>>')
             self.add_attribute(skill_to)
 
-    def check_resistance(self, damage_type, target=None):
+    def check_resistance(self, attack, target=None):
         """
         Checks if character has resistance.
 
         Args:
-            damage_type: Damage type to check resistance to.
+            attack: Attack to check resistance to.
             target: Check if specified target has resistance.
 
         Usage:
@@ -158,13 +153,22 @@ class Attributes:
             .check_resistance('resist pain', 'ranga')
         """
 
-        if target:
+        if self.is_str(target):
             target = self.get_object(target)
-        else:
+        if not target:
             target = self
 
         # Checks if character has resistance attribute
         for resist_name, resist_object in target.attributes['Resistance'].items():
-            for resist in resist_object.resistTypes:
-                if resist.lower() == attack.lower():
+            for resist in resist_object.resist_types:
+                if attack.damage_type in resist:
                     return True
+
+
+
+
+    def use_skill(self, skill):
+        try:
+            self.get_object(skill).use_skill()
+        except:
+            pass
