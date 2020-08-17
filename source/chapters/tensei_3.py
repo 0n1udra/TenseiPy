4b80d5d7104d3840acac3ff75354a4dda892cb72
Drake Thomas <drake550@pm.me>
2020-08-17 13:10:07 -0400

    consolidated to tensei_2

--- source/chapters/tensei_3.py
+++ source/chapters/tensei_3.py
@@ -2,43 +2,5 @@ from game_files.game_functions import *
 
 
 def Chapter3(rimuru):
-    class Chapter_3:
-        def __init__(self):
-            ssprint("*While practicing pronunciation with [Ultrasound Waves]. A pack of Star Wolfs shows up*")
-            ssprint("Was I that loud... Eh? Where are those wolves going? What's this...")
-            sprint("You strong one.")
-            ssprint("Wait, are those... Goblins? Also, How can I understand them?")
-            ssprint("<<Answer, [Magic Perception] converts sound waves to comprehensible sentences.>>")
-            ssprint("<<Also, sound waves can also be used to communicate your thoughts.>>")
-            ssprint("Is that so, let's try that.")
-            action_menu(self)
-
-
-        def _use_sticky_thread(self):
-            ssprint("Swinging from tree to tree with [Sticky Thread] seems to be a pretty effective way to travel.")
-
-        def _explore_more(self):
-            sprint("Where am I going?")
-            action_menu()
-
-
-
-
-    class _goblin_path:
-        def __init__(self):
-            ssprint("How should I introduce myself?")
-
-        def be_friendly(self):
-            sprint("HELLO, I'M A SLIME. MY NAME IS RIMURU")
-            sprint("...")
-            sprint("Strong one we have already recognized your strength. Please, lower your voice!")
-            ssprint("Oh. I guess my thoughts were to loud?")
-            sprint("Sorry, I am still adjusting.")
-            sprint("T-There is no need to apologize strong one!")
-            sprint("Ok, I wasn't planning on doing anything ahead of here. Is there anything you need from me?")
-            sprint("No, that is our village ahead of here. We felt a strong demonic aura and decided to immediately investigate.")
-            ssprint("Demonic aura? where?")
-            sprint("I didn't feel.")
-
-    Chapter_3()
+    pass
 
