5c9e961dc1dbea1010ffe46b71213467f4029899
Drake Thomas <drake550@pm.me>
2020-08-17 13:12:59 -0400

    in progress changing some story structural changes

--- source/chapters/tensei_2.py
+++ source/chapters/tensei_2.py
@@ -32,7 +32,6 @@ def Chapter2(rimuru):
 
         def _try_escaping(self):
             sprint("Alright, lets try out this new skill.")
-            add_level_mob(['giant bat', 'black spider', 'evil spider'])
             action_menu(_after_serpent())
 
         def _learn_water_bullet(self):
@@ -47,49 +46,32 @@ def Chapter2(rimuru):
 
     class _attack_serpent:
         def __init__(self):
+            add_level_mob(['giant bat', 'black spider', 'evil centipede'])
             ssprint("Now I have a way to attack.")
             ssprint("<Target the Tempest Serpent, then attack with your new skill. 'help' for more info.>")
             action_menu(self)
 
+        class _attack:
+            def __init__(self):
+
+                if not get_mob_status('tempest serpent'):
+                    sprint("Wow, what a powerful attack. I probably shouldn't use that so openly.")
+                    ssprint("<<Notice, would you like to use Unique Skill [Predator]?>>")
+                    sprint("Oh...? What will that do?")
+                    ssprint("<<Answer, after predation, information and targets skills may be obtained through analysis.>>")
+                    sprint("Are those bats?")
+                    ssprint("<<Answer, they are commonly known as [Giant Bat].>>")
+
+                if check_cleared_mobs():
+                    action_menu(_at_exit())
+                else:
+                    action_menu(_attack_serpent)
+
         def predate_ore(self): pass
-
-        def _attack(self):
-            if not get_mob_status('tempest serpent'):
-                sprint("Wow, what a powerful attack. I probably shouldn't use that so openly.")
-                ssprint("<<Notice, would you like to use Unique Skill [Predator]?>>")
-                sprint("Oh...? What will that do?")
-                ssprint("<<Answer, after predation, information and targets skills may be obtained through analysis.>>")
-                action_menu(_after_serpent())
-            else:
-                action_menu(self)
-
-    class _after_serpent:
-        def __init__(self):
-            add_level_mob(['giant bat', 'black spider'])
-            sprint("Are those bats?")
-            ssprint("<<Answer, they are commonly known as [Giant Bat].>>")
-            action_menu(self)
-
-        def predate_ore(self):
-            rimuru.add_inventory('magic ore')
-            ssprint("More of this, might be useful for later.")
-            action_menu(self)
 
         def _sneak_away(self):
             ssprint("I'll try to sneak away.")
             action_menu(_at_exit())
-
-        class _attack:
-            def __init__(self):
-                if get_mob_status('black spider'):
-                    sprint("What now...A Spider. Nobody likes spiders, especially giant ones.")
-
-                if get_mob_status('evil centipede'):
-                    sprint("For real, now a centipede. I just want to get out of this cave already.")
-                else:
-                    action_menu(_at_exit())
-
-                action_menu(_after_serpent)
 
     class _at_exit:
         def __init__(self):
@@ -108,7 +90,7 @@ def Chapter2(rimuru):
         def _sneak_out(self):
             ssprint("I'll try sneaking out after they go in.")
             sprint("Finally! I'm out of that cave. Where now to though?")
-            continue_story(rimuru, Chapter3)
+            action_menu(_the_encouter())
 
         def _say_hi(self):
             sprint("HELLO THERE!")
@@ -119,8 +101,97 @@ def Chapter2(rimuru):
             if rimuru.check_mob_has('veldora'):
                 ssprint("*After the little slime died. All of his stomach contents spewed outwards.*")
                 ssprint("*Unfortunately this particular slime had somehow absorbed a dragon locked in [Infinity Prison].*")
-                ssprint(
-                    "*The three adventurers where crushed by such a massive object. They Have failed there simple mission*")
+                ssprint("*The three adventurers where crushed by such a massive object. They Have failed there simple mission*")
             delete_game_save(rimuru)
 
+    class _the_encouter:
+        def __init__(self):
+            sprint("Where am I going?")
+            action_menu(self)
+
+        class _explore:
+            def __init__(self):
+                ssprint("*While practicing pronunciation with [Ultrasound Waves]. A pack of [Dire Wolves] shows up*")
+                ssprint("Was I that loud... Eh? Where are those wolves going? What's this...")
+                sprint("You strong one.")
+                ssprint("Wait, are those... Goblins? Also, How can I understand them?")
+                ssprint("<<Answer, [Magic Perception] converts sound waves to comprehensible sentences.>>")
+                ssprint("<<Also, sound waves can also be used to communicate your thoughts.>>")
+                ssprint("Is that so, let's try that.")
+                action_menu(_meet_goblins())
+
+            #def _try_escapeing(self):
+
+        if rimuru.check_mob_has('sticky thread'):
+            def _use_sticky_thread(self):
+                ssprint("Swinging from tree to tree with [Sticky Thread] seems to be a pretty effective way to travel.")
+                action_menu(_meet_goblins())
+
+    class _meet_goblins:
+        def __init__(self):
+            ssprint("How should I introduce myself?")
+
+        def _friendly(self):
+            sprint("HELLO, MY NAME IS RIMURU. I'M A SLIME.")
+            sprint("...")
+            sprint("Strong one we have already recognized your strength. Please, lower your voice!")
+            sprint("Sorry, I am still adjusting.")
+            sprint("T-There is no need to apologize strong one!")
+            sprint("I was just exploring around here. Is there something you guys need?")
+            sprint("No, you see, our village is ahead of here. We felt a strong demonic aura and decided to immediately investigate.")
+            ssprint("Demonic aura? what? I don't sense anything.")
+            ssprint("Great Sage can you set change my viewpoint, I want to see this 'demonic aura'.")
+            ssprint("OH!, so uh that's why everyone is drawn to me and why the wolves ran away at the sight of me. I should rein that in.")
+            ssprint("The goblins and I chatted some more then they invited me to there village.")
+            action_menu(_goblin_village)
+
+        def _ruthless(self):
+            sprint("Alright you weaklings. Listen here, all of you little shits can choose to follow my commands or death.")
+            sprint("So. What will it be?")
+            sprint("O-of course, we all obey!")
+            sprint("Now you guys got a base or village of some sort?")
+            sprint("Y-yes sir, our village is just up ahead. We would be delighted to have you.")
+            sprint("Alright, then lets get moving.")
+            action_menu(_goblin_village)
+
+    class _goblin_village:
+        global _help_goblins
+        def __init__(self):
+            ssprint("Wow, this place looks like a dump... ")
+            sprint("I am the village elder. I'm sorry we don't have much to serve you.")
+            sprint("So I'm guessing you didn't invite me here just for pleasantries.")
+            sprint("We've heard about your hidden strength. Would you please listen to our request.")
+            sprint("About a month ago our Dragon guardian disappeared, and nearby monsters have started expand there territory.")
+            sprint("There is a pack of 100 Dire Wolves that have been attacking us recently, and we are barely fendding them off.")
+            action_menu(self)
+
+        class _help_goblins:
+            def __init__(self):
+                sprint("Ok, I'll try the best of my abilities to protect your village.")
+                sprint("Thank you so much, we will be forever loyal to you.")
+
+            def _heal_wounded(self):
+                sprint("Show me your wounded")
+
+            def _setup_defenses(self):
+                sprint("Let's setup defenses.")
+
+        def _compensation(self):
+            sprint("So what, you want protection? What would my reward be?")
+            sprint("W-we don't have much to reward you with, but we can offer our unwavering loyalty.")
+            action_menu(_help_goblins())
+
+        def _leave(self):
+            sprint("I don't want anything to do with this. I'm just going to leave.")
+            sprint("Have we offended you in some way. Please we are desperate here!")
+            sprint("No, there's nothing that you can offer here that interests me.")
+
+
+    class _wolf_attack:
+        def __init__(self):
+            sprint("The Dire Wolves, they're here!")
+            sprint("Setup defenses")
+            ssprint("<Use the 'Command' command to tell subdoranites what to do.>")
+
+    #_the_encouter()
     chapter_2()
