0efe22d87068a5901351e7808ca3d00b8d55f38a
0n1udra <dt01@pm.me>
2020-10-12 22:19:00 -0400

    space refactoring, and moved around if statements in attack()

--- source/character_files/character_combat.py
+++ source/character_files/character_combat.py
@@ -3,7 +3,7 @@ class Combat:
         """
         Adds inputted targets to targeted_mobs list from user input.
 
-        Separates user inputted targets via ',' then checks to see if mob is in current_level_mobs list.
+        Separates user inputted targets by ',' then checks to see if mob is in current_level_mobs list.
         If so, adds to setTargets list.
 
         Args:
@@ -18,7 +18,7 @@ class Combat:
         else:
             for target in targets.split(','):
                 for i in self.current_level_mobs:
-                    # Checks if target is targetable by checking if in current_level_mobs list.
+                    # If can able to target, by checking if in current_level_mobs list.
                     if i.get_name() in target:
                         self.targeted_mobs.add(i)
 
@@ -26,10 +26,10 @@ class Combat:
         """
         Checks if can attack, and if it was successful.
 
-        First separates the targets and attacks via splitting up targets_and_attacks by commas ','.
+        First separates the targets and attacks by splitting up targets_and_attacks by commas ','.
         Then Adds the targets and attacks to corresponding lists.
-        Checks if target is not too high of a level for attack and doesn't have resistance to said attack.
-        Will return booleans for if attack was attempted and if attack was successful.
+        Checks if target is not too high of a level for attack, then if it has resistance to said attack.
+        Will return booleans if attack was attempted and  successful.
 
         Args:
             user_input: Targets to attack, if multiple, separated by comma ','.
@@ -47,28 +47,32 @@ class Combat:
         targets = self.targeted_mobs
         attack_success = False
 
-        # Tries to split up the inputted attacks.
+        # Checks if receiving multiple spells (attacks).
         try:
             attacks.append(attacks.split(','))
-        except AttributeError:
-            attacks.append(user_input)
+        except: attacks.append(user_input)
 
         # If mob is in current_level_mobs list and is is_alive, adds to focusTarget list.
         for attack in attacks:
-            attack = self.get_object(j)
+            attack = self.get_object(attack)
             if attack is not None:
                 skills.append(attack)
 
         for current_target in targets:
             for current_skill in skills:
-                if not self.check_resistance(current_skill, current_target):
-                    if current_target.level <= current_skill.damage_level:
-                        current_target.is_alive = False
-                        attack_success = True
-                        print(f"    < Eliminated {current_target.name}. >\n")
-                    else:
-                        print(f"    < {current_target.name} level too for that attack. >")
-                else:
+
+                # Checking if have resistance.
+                if self.check_resistance(current_skill, current_target):
                     print(f"    << Warning, {current_target.name} has resistance to {current_skill.damage_type}. >>")
+                    return False
+
+                # If target is lower level.
+                if current_target.level <= current_skill.damage_level:
+                    print(f"    < {current_target.name} level too for that attack. >")
+                    return False
+
+                current_target.is_alive = False
+                attack_success = True
+                print(f"    < Eliminated {current_target.name}. >\n")
 
         return attack_success
