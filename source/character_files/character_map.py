4c96b1b8cb09d9b0d8928615199d89f7e57ff285
0n1udra <dt01@pm.me>
2020-10-13 00:44:47 -0400

    made code that fetches location name easier to read

--- source/character_files/character_map.py
+++ source/character_files/character_map.py
@@ -1,28 +1,19 @@
 class Map:
-    def get_location_name(self, level_class=None):
+    def get_location_variable(self, level_class=None):
         # Looks for variable names that has __location in it, then uses eval to get the location variable data.
-        try:
-            location_name_index = [i for i, s in enumerate(dir(level_class)) if 'location_name' in s][0]
-        except: location = 'N/A'
-        else:
-            location = eval(f'level_class.{dir(level_class)[location_name_index]}')
-        return location
+        if level_class is None: return None
 
+        for function_name in dir(level_class):
+            if 'location' in function_name:
+                return eval(f"level_class.{function_name}")
 
-    def show_location(self, character=None):
-        character = self.get_object(character)
-        if not character: return
-
-        print(f"    < {character.name} location: {character.current_location}. >")
+    def get_location(self):
+        print(f"    < {self.name} location: {self.current_location}. >")
+        return self.current_location
 
     def get_map(self, location=None):
         print("map")
 
-    def update_location(self, new_location=None, character=None):
-        character = self.get_object(character)
-        if not character: character = self
-        if not character: return
-
-        if new_location in 'N/A': return
-
-        character.current_location = new_location
+    def update_location(self, new_location=None):
+        if not new_location: return False
+        self.current_location = new_location
