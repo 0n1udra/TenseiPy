6b8f2a705ddc17eb6203775dd52a63e9c06994bf
0n1udra <dt01@pm.me>
2021-01-23 13:45:44 -0500

    moves subs to game_locations

--- source/game_files/game_functions.py
+++ source/game_files/game_functions.py
@@ -5,15 +5,6 @@ import game_files.game_characters as mobs
 # I'm not exactly sure what actions will be taken in debug_mode, but so far it does get to the end of a chapter.
 rimuru = None
 fast_mode = False
-on_subs = ['activate', 'true', 'enable', 'on', '1']
-off_subs = ['deactivate', 'false', 'disable', 'off', '0']
-move_on_subs = ['explore', 'wonder', 'move', 'move on', 'move forward', 'keep moving', 'keep exploring', 'explore more', 'explore further', 'keep fumbling', 'fumble around more', 'fumble more', 'bounce around more', 'keep bouncing', 'just move on', 'keep moving forward', 'continue forward', 'continue exploring']
-wait_subs = ['wait', 'stay']
-activate_subs = ['activate', 'activate it', 'yes activate', 'yes activate it', 'yes please activate']
-do_it_subs = ['do it', 'yes do it', 'please do that', 'please do it', 'yes do that']
-yes_subs = ['yes', 'yes please', 'sure', 'yep', 'affirmative', 'affirmatory', 'i consent', 'i give my consent']
-all_yes_subs = activate_subs + do_it_subs + yes_subs
-no_subs = ['negative', "that's a negative", 'that is a negative', 'negatory', 'i refuse', 'denied', 'cancel', 'cancel it' 'no', 'no thanks', 'nah', "don't", "please don't", "don't do it", 'do not do that', 'do not']
 
 
 # start_game.py will load game save if user has one, if not it'll create one.
--- source/game_maps/game_location.py
+++ source/game_maps/game_location.py
@@ -1,5 +1,15 @@
 from game_files.game_functions import *
 
+on_subs = ['activate', 'true', 'enable', 'on', '1']
+off_subs = ['deactivate', 'false', 'disable', 'off', '0']
+move_on_subs = ['explore', 'wonder', 'move', 'move on', 'move forward', 'keep moving', 'keep exploring', 'explore more', 'explore further', 'keep fumbling', 'fumble around more', 'fumble more', 'bounce around more', 'keep bouncing', 'just move on', 'keep moving forward', 'continue forward', 'continue exploring']
+wait_subs = ['wait', 'stay']
+activate_subs = ['activate', 'activate it', 'yes activate', 'yes activate it', 'yes please activate']
+do_it_subs = ['do it', 'yes do it', 'please do that', 'please do it', 'yes do that']
+yes_subs = ['yes', 'yes please', 'sure', 'yep', 'affirmative', 'affirmatory', 'i consent', 'i give my consent']
+all_yes_subs = activate_subs + do_it_subs + yes_subs
+no_subs = ['negative', "that's a negative", 'that is a negative', 'negatory', 'i refuse', 'denied', 'cancel', 'cancel it' 'no', 'no thanks', 'nah', "don't", "please don't", "don't do it", 'do not do that', 'do not']
+
 class Game_Location:
     pass
 
