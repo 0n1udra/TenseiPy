b893535d84bfa7bfefcdb0e82c965f191968bef8
0n1udra <dt01@pm.me>
2021-02-18 02:42:25 -0500

    sorts list objects

--- source/game_files/extra.py
+++ source/game_files/extra.py
@@ -71,7 +71,7 @@ def format_info(name, var):
         return_data += f"{name}: "
 
     if type(var) is list:
-        return_data += ", ".join(var)
+        return_data += ", ".join(sorted(var))
     else:
         return_data += str(var)
 
