6792688063115aab295ac1d06735cfb817a29600
0n1udra <dt01@pm.me>
2021-03-09 04:05:31 -0500

    Updated items and character objects classes like I did with Skills

--- source/character_files/character.py
+++ source/character_files/character.py
@@ -10,86 +10,87 @@ from game_files.map import Map
 
 
 class Character(Info, Attributes, Inventory, Combat, Subordinates, Map):
+    # Main character attributes, inventory, skills, resistances, etc.
+    starting_state = []
+    friends = subordinates = {'Special S': {}, 'S': {}, 'Special A': {}, 'A+': {}, 'A': {},
+                              'A-': {}, 'B': {}, 'C': {}, 'D': {}, 'E': {}, 'F': {}, 'Other': {}}
+
+    attributes = {'Manas': {}, 'Ultimate Skill': {}, 'Unique Skill': {}, 'Extra Skill': {},
+                  'Intrinsic Skill': {},'Common Skill': {}, 'Skill': {}, 'Resistance': {}}
+
+    # Character inventory.
+    inventory = {'Item': {}, 'Material': {}, 'Consumable': {}, 'Living': {}, 'Weapon': {}, 'Misc': {}}
+    inventory_capacity = 0  # Inventory capacity in percentage.
+    inventory_capacity_add = 0.1  # Add to overall capacity when adding items to inventory.
+    quantity = 0  # Item quantity in inventory.
+    quantity_add = 1  # Usually items are added in batches, E.g. Hipokte Grass, Magical Ore.
+
+    # Character information and data related variables.
+    name = ''
+    family_name = ''
+    canon_name = ''  # Name from anime or manga storyline.
+    title = ''  # E.g. True Dragon, Demon Lord.
+    protections = []  # E.g. Storm Crest (from Veldora)>
+    shared_protection = ''  # Divine protection to be shared.
+    species = ''
+    rank = ''  # E.g. Catastrophe, Calamity.
+    level = 1  # Same as rank just as integer.
+    occupations = []
+    affiliations = []
+    info_page = ''  # Info page for character.
+    description = ''
+    appearance = ''  # Description of character appearance.
+    abilities = []
+    evolution = ''  # Species evolution of character.
+    acquired_msg = ''  # Message for when newly acquired.
+    is_alive = True
+    item_type = 'Living'
+    status = ''  # E.g. Passive, Active, Analysing, etc
+    data = {'kills': 0}  # Extra data, that I don't feel like need to be variables.
+    conditional_data = {}  # Contains data that player has done, paths taken, etc.
+
+    # Level/Map functionality.
+    active_mobs = []  # Current mobs around you that you can interact or attack.
+    targeted_mobs = []  # Targets that will be attacked with 'attack' command.
+    last_command = ''
+    last_skill = None  # Last successfully used skill, game object.
+    available_locations = []
+    current_location = ''
+    current_location_object = None  # Current location's class object.
+
+    # Game variables.
+    game_object_type = 'character'
+    save_path = ''
+    valid_save = None  # If you died in-game, the current save will be unusable.
+    textcrawl = None  # Slow text crawl effect, letter by letter.
+    show_hud = None  # Show available actions player can take.
+    show_art = True  # Show ASCII art. Disable to save room on screen.
+    show_hints = None  # If hardcore is True, this boolean will be ignored.
+    hardcore = None  # Hides targets, mimicking, and actions.
+    fast_mode = None
+    line_history = []  # So user can see the last x number of lines from game, if screen gets cluttered from other commands.
+
     def __init__(self):
-        # Main character attributes, inventory, skills, resistances, etc.
-        self.starting_state = []
-        self.friends = self.subordinates = {'Special S': {}, 'S': {}, 'Special A': {}, 'A+': {}, 'A': {},
-                                            'A-': {}, 'B': {}, 'C': {}, 'D': {}, 'E': {}, 'F': {}, 'Other': {}}
+        if self.starting_state: self.set_start_state()
+        self.update_info()
 
-        self.attributes = {'Manas': {}, 'Ultimate Skill': {}, 'Unique Skill': {}, 'Extra Skill': {}, 'Intrinsic Skill': {},
-                           'Common Skill': {}, 'Skill': {}, 'Resistance': {}}
-
-        # Character inventory.
-        self.inventory = {'Item': {}, 'Material': {}, 'Consumable': {}, 'Living': {}, 'Weapon': {}, 'Misc': {}}
-        self.inventory_capacity = 0  # Inventory capacity in percentage.
-        self.inventory_capacity_add = 0.1  # Add to overall capacity when adding items to inventory.
-        self.quantity = 0  # Item quantity in inventory.
-        self.quantity_add = 1  # Usually items are added in batches, E.g. Hipokte Grass, Magical Ore.
-
-        # Character information and data related variables.
-        self.name = ''
-        self.family_name = ''
-        self.canon_name = ''  # Name from anime or manga storyline.
-        self.title = ''  # E.g. True Dragon, Demon Lord.
-        self.protections = []  # E.g. Storm Crest (from Veldora)>
-        self.shared_protection = ''
-        self.species = ''
-        self.rank = ''  # E.g. Catastrophe, Calamity.
-        self.level = 1  # Same as rank just as integer.
-        self.occupations = []
-        self.affiliations = []
-        self.info_page = ''  # Info page for character.
-        self.description = ''
-        self.appearance = ''
-        self.abilities = []
-        self.evolution = ''
-        self.acquired_msg = ''
-        self.is_alive = True
-        self.item_type = 'Living'
-        self.status = ''  # E.g. Passive, Active, Analysing, etc
-        self.data = {'kills': 0}  # Extra data, that I don't feel like need to be variables.
-        self.conditional_data = {}  # Contains data that player has done, paths taken, etc.
-        self.game_object_type = 'character'
-
-        # Gameplay variables.
-        self.active_mobs = []  # Current mobs around you that you can interact or attack.
-        self.targeted_mobs = []  # Targets that will be attacked with 'attack' command.
-        self.last_command = ''
-        self.last_skill = None  # Last successfully used skill, game object.
-
-        # Map functionality.
-        self.available_locations = []
-        self.current_location = ''
-        self.current_location_object = None  # Current location's class object.
-
-        # Game variables.
-        self.save_path = ''
-        self.valid_save = None  # If you died in-game, the current save will be unusable.
-        self.textcrawl = None  # Slow text crawl effect, letter by letter.
-        self.show_hud = None  # Show available actions player can take.
-        self.show_art = True  # Show ASCII art. Disable to save room on screen.
-        self.show_hints = None  # If hardcore is True, this boolean will be ignored.
-        self.hardcore = None  # Hides targets, mimicking, and actions.
-        self.fast_mode = None
-        self.line_history = []  # So user can see the last x number of lines from game, if screen gets cluttered from other commands.
-
-    def __str__(self):
-        return self.name.lower()
+    def __str__(self): return self.name.lower()
 
     def set_start_state(self):
-        """ Adds corresponding starter attributes and items to character. """
+        """ Adds starter attributes and items to character. """
 
         for i in self.starting_state:
             self.add_attribute(i, show_acquired_msg=False)
 
     def get_object(self, match, item_pool=None, new=False, stricter=True):
         """
-        Can take in either a str or obj, then returns object (initialized if not already in inventory).
+        Can take in either a str or obj, then returns object (will initialize if need to).
 
         Args:
-            item_pool: List of game items to search against.
-            new: If first time adding a new object to character.
-            stricter: Find more of a exact match, still case insensitive. For example, sometimes Sage will be returned instead of Great Sage.
+            match str:
+            item_pool list: Add custom list of game items to search against.
+            new bool(False): Return new instance of object (already already not necessary).
+            stricter bool(True): Find more of a exact match, still case insensitive. For example, sometimes Sage will be returned instead of Great Sage.
 
         Returns:
             Corresponding object, will initialize if one hasn't been already in inventory.
@@ -105,16 +106,20 @@ class Character(Info, Attributes, Inventory, Combat, Subordinates, Map):
         if 'character' in self.game_object_type:
             item_pool = ([*self.inventory_generator(), *self.attributes_generator()])
 
+        # Create item_pool of all the game objects to be able to find match and return new instance of object if matched.
         if new is True:
             item_pool = ([*game_items.Item.__subclasses__(), *game_skills.Skill.__subclasses__(), *game_characters.Character.__subclasses__()])
 
         for game_object in item_pool:
+            # Somehow strings get in the item_pool, need to filter those out.
             if type(game_object) is str: continue
 
+            # If need to return a new instance of object.
             if new:
                 try: game_object = game_object()
                 except: pass
 
+            # Find more exact name match.
             if stricter:
                 if str(game_object).lower() == str(match).lower().strip():
                     return game_object
@@ -126,21 +131,26 @@ class Character(Info, Attributes, Inventory, Combat, Subordinates, Map):
         return None
 
     def check_acquired(self, check_object, amount=1):
-        """ Checks if you pass in a list of strings or just a single string. """
+        """
+        Checks if you pass in a list of strings or just a single string.
+
+        Args:
+            check_object str: Object to check if acquired.
+            amount int(1): Base amount to check if own.
+        """
 
         if type(check_object) is list:
             for i in check_object:
                 return self._check_acquired(i, amount)
-        else:
-            return self._check_acquired(check_object, amount)
+        else: return self._check_acquired(check_object, amount)
 
     def _check_acquired(self, check_object, amount=1):
         """
         Checks to see if character has object/attribute/item/etc.
 
         Args:
-            check_object: Object to check if specified character has item, attribute, skill, etc.
-            amount [int:1]: Check quaintly of item, only works for inventory right now.
+            check_object str: Object to check if specified character has item, attribute, skill, etc.
+            amount int(1): Check quaintly of item, only works for inventory right now.
 
         Returns:
             Boolean: If specified character has attribute or object.
--- source/game_files/items.py
+++ source/game_files/items.py
@@ -1,22 +1,24 @@
 from game_files.extra import format_info
 
 class Item:
+    name = ''
+    status = ''
+    item_type = ''
+    damage_type = ''
+    damage_level = None
+    quantity = 0
+    quantity_add = 1
+    inventory_capacity_add = 0.1
+    info_page = 'N/A'
+    description = ''
+    usage = ''
+    appearance = ''
+    recipe = {}
+    ingredient_for = []
+    game_object_type = 'item'
+
     def __init__(self):
-        self.name = ''
-        self.status = ''
-        self.item_type = ''
-        self.damage_type = ''
-        self.damage_level = None
-        self.quantity = 0
-        self.quantity_add = 1
-        self.inventory_capacity_add = 0.1
-        self.info_page = 'N/A'
-        self.description = ''
-        self.usage = ''
-        self.appearance = ''
-        self.recipe = {}
-        self.ingredient_for = []
-        self.game_object_type = 'item'
+        self.update_info()
 
     def get_description(self):
         """
@@ -31,24 +33,26 @@ class Item:
     def update_info(self):
         """ Updates item's info. """
 
+        # If attribute/skill has a special status.
         self.info_page = f'    Name: [{self.name}] {"(" + self.status + ")" if self.status else ""}\n'
 
+        # Only show fields that have set data.
         info_dict = {'Item Type': self.item_type, 'Damage Type': self.damage_type, 'Damage Level': self.damage_level,
                      '*Description': self.description, '*Appearance': self.appearance, '*Usage': self.usage}
         for k, v in info_dict.items():
             if formatted_info := format_info(k, v):
                 self.info_page += f"    {formatted_info}\n"
 
+        # If this item is part of another items requirements/recipe.
         if self.ingredient_for:
             self.info_page += "\n    Ingredient for:\n"
-
             for item in self.ingredient_for:
                 self.info_page += f'        [{item}], '
             self.info_page = self.info_page[:-2] + '\n'
 
+        # Show ingredients/prerequisites/requirements to be craft item.
         if self.recipe:
             self.info_page += f'\n    Recipe:\n        Will craft {self.quantity_add} at a time.\n'
-
             for item, amount in self.recipe.items():
                 self.info_page += f'        {amount}x [{item}]\n'
 
@@ -67,79 +71,61 @@ class Item:
 
 # ========== Materials ==========
 class Water(Item):
-    def __init__(self):
-        Item.__init__(self)
-        self.name = 'Water'
-        self.item_type = 'Material'
-        self.quantity_add = 100
-        self.inventory_capacity_add = 0.01
-        self.usage = 'Can be used in high pressure attacks.'
-        self.description = '''It's wet, and it's clear, it is just water...'''
-        self.appearance = 'Clear flowing liquid.'
-        self.update_info()
+    name = 'Water'
+    item_type = 'Material'
+    quantity_add = 100
+    inventory_capacity_add = 0.01
+    usage = 'Can be used in high pressure attacks.'
+    description = '''It's wet, and it's clear, it is just water...'''
+    appearance = "Clear flowing liquid, oh, and it's wet too!"
 
 class Hipokte_Grass(Item):
-    def __init__(self):
-        Item.__init__(self)
-        self.name = 'Hipokte Grass'
-        self.item_type = 'Material'
-        self.quantity_add = 50
-        self.inventory_capacity_add = 0.01
-        self.usage = 'Mainly used for making healing potions.'
-        self.description = 'Magicule infused grass, found in locations with high Magicule concentration'
-        self.appearance = 'Looks like regular grass, but gives off small amounts of magic essence.'
-        self.ingredient_for = ['Full Potion']
-        self.update_info()
+    name = 'Hipokte Grass'
+    item_type = 'Material'
+    quantity_add = 50
+    inventory_capacity_add = 0.01
+    usage = 'Mainly used for making healing potions.'
+    description = 'Magicule infused grass, found in locations with high Magicule concentration'
+    appearance = 'Looks like regular grass, but gives off small amounts of magic essence.'
+    ingredient_for = ['Full Potion']
 
 class Magic_Ore(Item):
-    def __init__(self):
-        Item.__init__(self)
-        self.name = 'Magic Ore'
-        self.item_type = 'Material'
-        self.quantity_add = 25
-        self.inventory_capacity_add = 0.1
-        self.usage = 'Mainly used for making magic items and magic reinforced weapons and armor.'
-        self.description = '''Magic ores form around high concentration of magic essence.
-        Magic ore is the raw form of magic steel. Even in its unrefined form, magic ore is considered to be valuable.
-        '''
-        self.appearance = 'A very colorful ore. Almost like a glowing shimmering rainbow effect, while giving off some magic essence.'
-        self.update_info()
+    name = 'Magic Ore'
+    item_type = 'Material'
+    quantity_add = 25
+    inventory_capacity_add = 0.1
+    usage = 'Mainly used for making magic items and magic reinforced weapons and armor.'
+    description = '''Magic ores form around high concentration of magic essence.
+    Magic ore is the raw form of magic steel. Even in its unrefined form, magic ore is considered to be valuable.
+    '''
+    appearance = 'A very colorful ore. Almost like a glowing shimmering rainbow effect, while giving off some magic essence.'
 
 class Full_Potion(Item):
-    def __init__(self):
-        Item.__init__(self)
-        self.name = 'Full Potion'
-        self.item_type = 'Consumable'
-        self.quantity_add = 25
-        self.inventory_capacity_add = 0.1
-        self.usage = 'Heals major wounds even severed limbs and illnesses. However, can not resurrect.'
-        self.description = '''Can heal subject to optimum condition, but cannot revive the dead.'''
-        self.appearance = 'Blue potion in a glass bottle.'
-        # One Hipokte Grass makes 10 potions.
-        self.recipe = {'Hipokte Grass': 1}
-        self.update_info()
+    name = 'Full Potion'
+    item_type = 'Consumable'
+    quantity_add = 25
+    inventory_capacity_add = 0.1
+    usage = 'Heals major wounds even severed limbs and illnesses. However, can not resurrect.'
+    description = '''Can heal subject to optimum condition, but cannot revive the dead.'''
+    appearance = 'Blue potion in a glass bottle.'
+    # One Hipokte Grass makes 10 potions.
+    recipe = {'Hipokte Grass': 1}
 
 # ========== Weapons ==========
 class Magic_Sword(Item):
-    def __init__(self):
-        Item.__init__(self)
-        self.name = "Magic Sword"
-        self.item_type = 'Weapon'
-        self.damage_type = 'Melee'
-        self.damage_level = 6
-        self.description = """If a huge amount of magic essence was included in the raw materials of a sword, apparently it would become a sword that grows. 
-        Infusing magic into a sword, though you'd expect it to be a common technique, it's actually really difficult.
-        """
-        self.appearance = """Although they are usually made using a simple design, they are perfectly straight.
-        It could be said that there are no unnecessary parts to them.
-        It isn't meant for pure cutting, but still capable of slashing attacks.
-        The goal of this design is to use this simplicity as a base to make it easier to realize an individual's respective ideal form.
-        
-        Another special characteristic of magic swords is that the blade will never rust or become chipped.
-        The sword also has a life of its own.
-        If it became completely broken or bent, then the magic essence would bleed out and dissipate all at once.
-        """
-        self.update_info()
-
-
-
+    name = "Magic Sword"
+    item_type = 'Weapon'
+    damage_type = 'Melee'
+    damage_level = 6
+    description = """If a huge amount of magic essence was included in the raw materials of a sword, apparently it would become a sword that grows. 
+    Infusing magic into a sword, though you'd expect it to be a common technique, it's actually really difficult.
+    """
+    appearance = """Although they are usually made using a simple design, they are perfectly straight.
+    It could be said that there are no unnecessary parts to them.
+    It isn't meant for pure cutting, but still capable of slashing attacks.
+    The goal of this design is to use this simplicity as a base to make it easier to realize an individual's respective ideal form.
+    
+    Another special characteristic of magic swords is that the blade will never rust or become chipped.
+    The sword also has a life of its own.
+    If it became completely broken or bent, then the magic essence would bleed out and dissipate all at once.
+    """
