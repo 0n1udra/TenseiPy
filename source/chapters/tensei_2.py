a6ee63a17e455244bdb159a4a78e36542a86db53
0n1udra <dt01@pm.me>
2021-05-02 01:30:19 -0400

    direwolf attack story touch up

--- source/chapters/tensei_2.py
+++ source/chapters/tensei_2.py
@@ -16,33 +16,37 @@ def ch2_goblin_encounter(rimuru):
             siprint("\nWho's that? Wait... They look like goblins! Should I be nice?")
             game_action(self)
 
-        class _friendly:
+        class _be_friendly:
             __subs = subs.be_nice_subs + ['my name is rimuru', 'say hi', 'talk', 'chat', 'talk to goblins']
             def __init__(self):
                 sprint("HELLO, MY NAME IS RIMURU. I'M A SLIME.")
                 sprint("...")
                 sprint("Strong one we have already recognized your strength. Please, lower your voice!")
                 sprint("Oh, ok. I was just exploring around here. Is there something you need?")
-                sprint("No, you see, our village is near. We felt a strong demonic aura and decided to immediately investigate.")
-                siprint("\nDemonic aura? what? Great Sage can you change my viewpoint, I want to see this demonic aura.")
-                siprint("OH!, so uh that's why everyone is drawn to me and why the wolves ran away at the sight of me. I should rein that in.")
-                siprint("After some more clarification, they invited to their village. It looks like they need some help.")
+                if rimuru.check_acquired('veldora'):
+                    sprint("No, you see, our village is near. We felt a strong demonic aura and decided to immediately investigate.")
+                    siprint("\nDemonic aura? What? Great Sage can you change my viewpoint, I want to see this demonic aura.")
+                    siprint("OH!, so uh that's why everyone is drawn to me and why the wolves ran away at the sight of me. I should rein that in.")
+                else:
+                    sprint("We were just patrolling our village borders. We now recognize you are not just another simple slime.")
+                siprint("\nThey're inviting me to their village. It looks like they need some help.")
                 goto_goblin_village()
 
         class _subjugate_goblins:
             __subs = ['subjugate', 'subjugate goblins', 'rule goblins', 'ruthless', 'be ruthless', 'enslave', 'enslave goblins']
             def __init__(self):
-                sprint("\nAlright you weaklings, listen here you little shits, I'll only say this once!")
-                sprint("You have two options. You can worship me or you can die.")
-                dots(2, 10)
+                sprint("Alright you weaklings, listen here you little shits, I'll only say this once!")
+                sprint("You have two options. You can worship me or you can die.\n")
+                dots(5, 2)
                 if get_random(1, 10, 1):  # 1/10 chance of goblins revolting
                     sprint("\nNEVER! We will never surrender to you!")
                     sprint("ATTACK!!!!!!!!")
                     game_over()
 
+                sprint("\nWe offer our loyalty to you strong one!")
                 sprint("\nOk, good choice. So, you guys have a base, village, anything?")
                 sprint("Y-yes sir, our village is just up ahead. We would be delighted to have you.")
-                sprint("Of course you would. Lets start moving.")
+                sprint("Of course you would. Lets start moving.\n")
                 goto_goblin_village()
 
         class hfunc_attack:
@@ -59,10 +63,10 @@ def ch2_goblin_encounter(rimuru):
 
         def __init__(self):
             mobs_reset()
-            siprint("\nWow, this place looks like a dump... ")
-            sprint("\nI am the village elder. I'm sorry we don't have much to serve you.")
-            sprint("So I'm guessing you didn't invite me here just for pleasantries.")
-            sprint("We've heard about your hidden strength. Would you please listen to our request.")
+            siprint("Wow, this place looks like a dump... ")
+            sprint("\nI am the village elder. I'm sorry we don't have much to offer you.")
+            sprint("\nSo I'm guessing you didn't invite me here just for pleasantries.")
+            sprint("\nWe've heard about your hidden strength. Would you please listen to our request.")
             sprint("About a month ago our Dragon guardian disappeared, and nearby monsters have started expand there territory.")
             sprint("There is a pack of 100 Dire Wolves that have been attacking us recently, and we are barely fending them off.")
             siprint("\nHmmmmm... Should I help them? What about compensation?")
@@ -83,16 +87,17 @@ def ch2_goblin_encounter(rimuru):
                 game_action(self)
 
             class _setup_defenses:
-                __subs = ['go setup defenses', 'setup some defenses', 'setup border defenses']
+                __subs = ['setup', 'go setup defenses', 'setup some defenses', 'setup border defenses']
                 def __init__(self):
                     sprint("Hey! Get some goblins to setup defenses around the parameter.")
+                    sprint("Setup some fences, and start training anyone that can do battle!")
 
-                    if rimuru.check_acquired('sticky thread'):
-                        siprint("I can use [Sticky Thread] as a trap also.")
-                        game_cond('sticky thread trap', True)
                     if rimuru.check_acquired('steel thread'):
-                        siprint("Perhaps [Steel Thread] can be use as a last line of defense.")
+                        siprint("I can use [Steel Thread] as invisible traps.")
                         game_cond('steel thread trap', True)
+                    if rimuru.check_acquired('sticky thread'):
+                        siprint("I wonder what I can do with [Sticky Thread].")
+                        game_cond('sticky thread trap', True)
 
                     wolf_attack()
 
@@ -109,57 +114,67 @@ def ch2_goblin_encounter(rimuru):
                         action_playable('_heal_wounded', False)
                     else:
                         siprint("Looks like 9 wounded goblins..... How can I help them?")
-                        sprint("I need some way to heal them.")
+                        siprint("I need some way to heal them.")
 
     class wolf_attack:
         def __init__(self):
-            mobs_add(['direwolf leader'])
+            sprint("\n* Meanwhile... *")
+            sprint("\nIt is a full and bright moon tonight.")
+            sprint("Tonight we shall lay waste to the goblin village.")
+            sprint("And take our first step towards conquering The Forest of Jura!")
+            sprint("They no longer have protection from that accursed dragon!")
+
             sprint("\nMaster! The Direwolves are here!")
-            sprint("\nWe are the Direwolves! And I am they're leader!")
-            sprint("This village shall be soaked in goblin blood soon enough!")
             game_action(self)
 
         class _give_warning:
             __subs = ['give them a warning', 'take over', 'take over as king', 'become new king', 'subjugate goblins', 'subjugate them']
             def __init__(self):
-                sprint("Listen up, because I'm only going to say this once!")
+                mobs_add(['direwolf leader'])
+                sprint("Stop where you are! Listen up, because I'm only going to say this once!")
                 sprint("Acknowledge me as your king, or retreat now and never show yourselves again!")
-                sprint('\nOur wolf pack will not be initimdated by a mere slime!')
+                sprint('\nOur wolf pack will not be intimidated by a mere slime!')
                 sprint("ATTACK!!!")
-                siprint("\nIntimidating not work...")
-
-                if game_cond('sticky thread trap'):
-                    siprint("Those [Sticky Thread] traps are working well I see.")
-                    sprint("\nYour tricks will not stop us!")
+                siprint("\nIntimidating not working...")
+                siprint("Some of the goblins that are using bows are getting a few of them, but it's not enough.")
 
                 if game_cond('steel thread trap'):
-                    sprint("W-w-what is this!?")
-                    sprint("\nThat would be [Steel Thread]! Can't bite through that can you!")
+                    sprint("\nWhat was that!")
+                    sprint("That would be [Steel Thread].")
+                    sprint("\nYour tricks will not stop us!")
+
+                siprint("\nSome are still getting past my traps.")
+
+                sprint("\nEnough of this! I will kill you slime!")
+                sprint("\nFather! No!")
+                if game_cond('steel thread trap'):
+                    sprint("W-w-what is this! Why can't I move!")
+                    sprint("\nThat would be [Steel Thread]!")
                 # Without setting up steel thread as defense, the pack leader will kill you!
                 else:
                     siprint("\nThat wolf is coming at me way to fast! I have nothing to stop him!")
                     sprint("\nYOU'RE DEAD, YOU SLIME!")
                     sprint("\n*CHOMP*")
                     game_over()
-
                 set_targets('direwolf leader')
                 game_action(self)
 
             class _attack_water_blade:
                 __subs = []
                 def __init__(self):
-                    siprint("Perhaps they'll take me more seriously if I take out the leader.")
                     if mobs_cleared():
-                        siprint("I did it! The leader is dead! Maybe they will listen if I...")
+                        sprint("No, Dad!")
+                        sprint("\nYour leader is dead. Your choice now is fealty or death!")
+                        siprint("They're not doing anything now... What are they waiting for? A leader perhaps?")
                     else:
-                        siprint("\nHow the leader survive!?")
+                        siprint("\nHow did he survive!?")
                         game_over()
 
                     game_action(self)
 
                 class _eat:
                     def __init__(self):
-                        siprint("What can I do to possibly coerce them?")
+                        sprint("\nMaybe they need a push...")
                         game_action(self)
 
                     class _mimic_direwolf_leader:
@@ -170,6 +185,7 @@ def ch2_goblin_encounter(rimuru):
                         class _use_coercion:
                             def __init__(self):
                                 sprint("HOOWWWLLLLLL!!!!!..........")
+                                siprint("I know I said fealty or death, but I'm hoping they'll just run away...")
                                 dots(5)
                                 sprint("HOWWLLLL!!!!......")
                                 sprint("\nW-we surrender.")
